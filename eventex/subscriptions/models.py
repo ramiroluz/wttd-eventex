3683b1ca3e1cffd518bb6305f9f398d431b48b79
Ramiro Batista da Luz <ramiroluz@gmail.com>
2016-09-26 22:13:27 -0300

    Corrige created_at para auto_now_add.

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -6,7 +6,7 @@ class Subscription(models.Model):
     cpf = models.CharField('CPF', max_length=11)
     email = models.EmailField('e-mail')
     phone = models.CharField('telefone', max_length=20)
-    created_at = models.DateTimeField('criado em', auto_now=True)
+    created_at = models.DateTimeField('criado em', auto_now_add=True)
     paid = models.BooleanField('pago', default=False)
 
     class Meta:
