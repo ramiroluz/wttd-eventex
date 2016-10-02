525204d3587b07c88859892534081027f1180a7b
Ramiro Batista da Luz <ramiroluz@gmail.com>
2016-10-02 20:17:42 -0300

    Simplifica com modelform.

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,14 +1,11 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
+from eventex.subscriptions.models import Subscription
+from eventex.subscriptions.validators import validate_cpf
 
-def validate_cpf(value):
-    if not value.isdigit():
-        raise ValidationError('CPF deve conter apenas números', 'digits')
-    if len(value) != 11:
-        raise ValidationError('CPF deve ter 11 dígitos', 'length')
 
-class SubscriptionForm(forms.Form):
+class SubscriptionFormOld(forms.Form):
     name = forms.CharField(label='Nome')
     cpf = forms.CharField(label='CPF', validators=[validate_cpf])
     email = forms.EmailField(label='Email', required=False)
@@ -26,6 +23,29 @@ class SubscriptionForm(forms.Form):
         if not email and not phone:
             raise ValidationError('Informe seu e-mail ou telefone.')
         return self.cleaned_data
+
+    def normalize_word(self, word):
+        return word.capitalize() if len(word) > 2 else word.lower()
+
+
+class SubscriptionForm(forms.ModelForm):
+    class Meta:
+        model = Subscription
+        fields = ['name', 'cpf', 'email', 'phone']
+
+    def clean(self):
+        self.cleaned_data = super().clean()
+        email = self.cleaned_data.get('email')
+        phone = self.cleaned_data.get('phone')
+        if not email and not phone:
+            raise ValidationError('Informe seu e-mail ou telefone.')
+        return self.cleaned_data
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        words = [self.normalize_word(word) for word in name.split()]
+        capitalized = ' '.join(words)
+        return capitalized
 
     def normalize_word(self, word):
         return word.capitalize() if len(word) > 2 else word.lower()
